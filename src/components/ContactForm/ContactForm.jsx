7bd057c7b13679f94de2fc3b9a54b07b411489fc
Gigapatut <125769111+Gigapatut@users.noreply.github.com>
2023-09-24 09:11:22 +0300

    hw-02-phonebook

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -35,8 +35,6 @@ class ContactForm extends React.Component {
           <input
             type="text"
             name="name"
-            pattern="[A-Za-z\s]{1,15}"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
             value={this.state.name}
             onChange={this.handleChange}
@@ -47,8 +45,6 @@ class ContactForm extends React.Component {
           <input
             type="tel"
             name="number"
-            pattern="[0-9]{3}-[0-9]{2}-[0-9]{2}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
             value={this.state.number}
             onChange={this.handleChange}
